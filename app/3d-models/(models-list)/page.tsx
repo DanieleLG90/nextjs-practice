befc1aac48050f99d7dce865d544c847c0e4d947
Daniele Lorenzo Giacomazzi <giacomazzi06@gmail.com>
2026-03-05 12:11:42 +0100

    3d models page

--- app/3d-models/(models-list)/page.tsx
+++ app/3d-models/(models-list)/page.tsx
@@ -1,4 +1,8 @@
 import modelsData from "@/app/data/models.json"
+import Link from "next/link"
+
+import Image from "next/image"
+import HeroImgSquare from "@/public/hero-image-square.png"
 
 type Model = {
   id: number
@@ -10,18 +14,30 @@ type Model = {
   dateAdded: string
 }
 
-
-
 export default async function ModelsPage3D () {
 
     const models: Model[] = modelsData
 
     return(
-        <div className="flex flex-col">
+        <div className="p-3 flex flex-col gap-3">
             {models.map((item: Model)=>(
+<<<<<<< Updated upstream
                 <div key={item.id}>
                     <h1>{item.name}</h1>
                 </div>
+=======
+                <Link href= "" key={item.id} className="flex flex-col border-2 border-gray-200 rounded-md shadow">
+                    <Image className="rounded-t-sm" src={HeroImgSquare} alt="img with 3d printing items" />
+                    <div className="p-2 flex flex-col gap-2">
+                        <h3 className="text-xl font-semibold">{item.name}</h3>
+                        <p>{item.description}</p>
+                        <p className="w-fit p-1 px-2 border border-gray-200 rounded-full">{item.category}</p>
+                        <div>
+                            <p>{item.likes}</p>
+                        </div>
+                    </div>
+                </Link>
+>>>>>>> Stashed changes
             ))}
         </div>
         
